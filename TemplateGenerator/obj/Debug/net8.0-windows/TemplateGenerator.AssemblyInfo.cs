5ae0953a1b62fdc73b71358eb9114c227fe5556a
Решетов Антон Николаевич <reshetov@teconpc.local>
2026-08-10 12:53:39 +0300

    Генератор экземпляров по шаблону для POU

--- TemplateGenerator/obj/Debug/net8.0-windows/TemplateGenerator.AssemblyInfo.cs
+++ TemplateGenerator/obj/Debug/net8.0-windows/TemplateGenerator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TemplateGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe1c3c1a2c948927f6d8faa028401db7c4ac9832")]
 [assembly: System.Reflection.AssemblyProductAttribute("TemplateGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TemplateGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
