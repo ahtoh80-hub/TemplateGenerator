3edc521581ec1281a5b7906909665828d668651d
Антон Решетов <reshetov@teconpc.local>
2026-08-14 17:40:57 +0300

    Обновление: 0

--- TemplateGenerator/obj/Debug/net8.0-windows/TemplateGenerator.AssemblyInfo.cs
+++ TemplateGenerator/obj/Debug/net8.0-windows/TemplateGenerator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TemplateGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0784b29bc95564c600555a63341ea60ae664f43d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdc7bb54182c11dfac91d568e7caf20f72358505")]
 [assembly: System.Reflection.AssemblyProductAttribute("TemplateGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TemplateGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
